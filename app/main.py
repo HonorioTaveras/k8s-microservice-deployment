d902edae3a33d83bb402a022fe27939db79a1968
Honorio Taveras <htaveras90@gmail.com>
2025-11-03 22:20:35 -0700

    Wiring metrics to app/main.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import logging
 from fastapi import FastAPI
-from app.routers import info
+from app.routers import info, metrics
 
 def create_app() -> FastAPI:
     app = FastAPI(title="metrics-api")
     app.include_router(info.router)
+    app.include_router(metrics.router)
     return app
 
 app = create_app()
